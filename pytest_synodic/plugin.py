ebd9d204ca05bf82e0edcd9d997c9589a5a0780e
Asher Norland <asher.norland@gmail.com>
2024-01-11 17:49:48 -0500

    Update Plugin Base

--- pytest_synodic/plugin.py
+++ pytest_synodic/plugin.py
@@ -2,6 +2,7 @@
 
 from abc import ABCMeta
 from importlib.metadata import entry_points
+from typing import LiteralString
 
 import pytest
 from synodic_utilities.utility import canonicalize_type
@@ -14,22 +15,29 @@ class BaseTests[PluginT](metaclass=ABCMeta):
     def fixture_plugin_type(self) -> type[PluginT]:
         """A required testing hook that allows type generation. The user should override this fixture and return their custom plugin type"""
 
-        raise NotImplementedError("Override this fixture")
+        raise NotImplementedError("Override this fixture in the plugin module")
+
+    @pytest.fixture(name="plugin_group_name", scope="session")
+    def fixture_plugin_group_name(self) -> LiteralString:
+        """A required testing hook that provides identifying information. The user should override this fixture and return the base group name for their plugin type"""
+
+        raise NotImplementedError("Override this fixture in the base test class")
 
 
 class IntegrationTests[PluginT](metaclass=ABCMeta):
     """Integration testing information for all plugin test classes"""
 
-    def test_entry_point(self, plugin_type: type[PluginT]) -> None:
+    def test_entry_point(self, plugin_type: type[PluginT], plugin_group_name: LiteralString) -> None:
         """Verify that the plugin was registered
 
         Args:
             plugin_type: The type to register
+            plugin_group_name: The group name for the plugin type
         """
         group = canonicalize_type(plugin_type).group
 
         types = []
-        for entry in list(entry_points(group=f"cppython.{group}")):
+        for entry in list(entry_points(group=f"{plugin_group_name}.{group}")):
             types.append(entry.load())
 
         assert plugin_type in types
